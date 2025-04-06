9c0afc36aa27bf25220cf46c6758e81e56362095
uuuvn <uuuvn@proton.me>
2025-04-07 00:35:58 +0500

    misc cleanup

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -xeo pipefail
 rm -rf dist/*
+export GHIDRA_INSTALL_DIR=$HOME/src/ghidra_11.3.1_PUBLIC
 gradle buildExtension
 FILE="$PWD/dist/*"
 cd ~/Library/ghidra/ghidra_11.3.1_PUBLIC/Extensions
